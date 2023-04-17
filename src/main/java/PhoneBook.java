7c2c51e1b9edd596fbacceabea3023e44f9aac97
Shot <shot89@gmail.com>
2023-04-17 10:07:33 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,7 @@
 public class PhoneBook {
+
+    public int add(String number, String name){
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 
 class PhoneBookTest {
+    PhoneBook phoneBook;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Test start");
+        phoneBook = new PhoneBook();
+    }
+
+    @ParameterizedTest
+    @MethodSource("addSource")
+    void test_add(int expected, String phone, String name){
+        Assertions.assertEquals(expected, phoneBook.add(phone,name));
+    }
+
+    private static Stream<Arguments> addSource(){
+        return Stream.of(
+                Arguments.of(1, "89999999999","admin"),
+                Arguments.of(-1, "89999999999","admin")
+        );
+    }
 
 }
